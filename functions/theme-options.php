229849d8c74d46eb7ae592997d772872a8264064
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-03-23 19:08:37 +0100

    Remove ?filter=5 from theme review link

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -24,7 +24,7 @@ Kirki::add_section( 'reviewlink', array(
 	'panel'       => 'options',
 	'type'        => 'link',
 	'button_text' => esc_html__( 'Write a Review', 'readline' ),
-	'button_url'  => 'https://wordpress.org/support/theme/readline/reviews/?filter=5#new-post',
+	'button_url'  => 'https://wordpress.org/support/theme/readline/reviews/#new-post',
 	'priority'    => 1,
 ) );
 
